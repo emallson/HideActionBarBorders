62c7e6cdd7de79797f9431b048f9ab5214a2a888
J. David Smith <emallson@archlinux.us>
2022-10-31 18:26:00 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# HideActionBarBorders
+
+![example action bar with borders hidden](./readme/example.png)
+
+A very small and efficient addon to hide the borders of the default action bars in World of Warcraft: Dragonflight.
+
+This will likely interact badly with other action bar addons (including ElvUI) and should only be used with default action bars.
+
+## Getting Square Icons
+
+To get clean, square icons as shown in the screenshot above, download and install [Clean Icons - Mechagnome Edition](https://www.wowinterface.com/downloads/info25064-CleanIcons-MechagnomeEdition.html). This is a no-code icon pack that will not impact your game's performance.
+
+# License
+
+Copyright 2022 emallson.
+
+Published under the BSD 3-clause license. See [LICENSE](./LICENSE) for details.
---
+++ src/main.lua
@@ -0,0 +1,53 @@
+local bars = {
+    "Action",
+    "MultiBarBottomLeft",
+    "MultiBarBottomRight",
+    "MultiBarLeft",
+    "MultiBarRight",
+    "MultiBar5",
+    "MultiBar6",
+    "MultiBar7",
+    "PetAction",
+}
+
+local function ButtonCount(bar)
+    if bar == "PetAction" then
+        return 10
+    else
+        return 12
+    end
+end
+
+local function HideBorder(button)
+    button.NormalTexture:Hide()
+    button.IconMask:Hide()
+end
+
+local function AdjustButtonOverlays(button)
+    button.cooldown:SetAllPoints(button)
+end
+
+local function HideSelf(self)
+    self:Hide()
+end
+
+local function setup()
+    for _, bar in ipairs(bars) do
+        for i = 1,ButtonCount(bar) do
+            local key = bar .. "Button" .. i
+            local button = _G[key]
+            if button ~= nil then
+                HideBorder(button)
+                button:HookScript('OnShow', HideBorder)
+                AdjustButtonOverlays(button)
+                button.NormalTexture:HookScript("OnShow", HideSelf)
+            else
+                print("Missing expected button: " .. key)
+            end
+        end
+    end
+end
+
+-- this addresses a race condition with applying the above to the default action bar. 
+-- naively calling `setup` frequently causes the borders to be hidden on all bars *except* the main bar.
+C_Timer.After(0, setup)
