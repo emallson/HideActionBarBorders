953e1922846eceeeb4c2c0a3cd2e6b88c3d2687e
J. David Smith <emallson@archlinux.us>
2022-11-13 21:28:13 -0500

    add fatpala's highlight texture

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ This will likely interact badly with other action bar addons (including ElvUI) a
 
 To get clean, square icons as shown in the screenshot above, download and install [Clean Icons - Mechagnome Edition](https://www.wowinterface.com/downloads/info25064-CleanIcons-MechagnomeEdition.html). This is a no-code icon pack that will not impact your game's performance.
 
+## Highlight Changes
+
+This includes an updated highlight texture courtesy of `@Fatpala` that makes the mouseover, keypress, and item highlights line up with square icons. I do not currently have a way of *detecting* whether you're using square icons, so this is on by default.
+
 # License
 
 Copyright 2022 emallson.
--- src/main.lua
+++ src/main.lua
@@ -24,8 +24,43 @@ local function HideBorder(button)
     button.IconMask:Hide()
 end
 
+local replacementTexture = "interface/addons/HideActionBarBorders/resources/uiactionbar2x"
+
+local replacementAtlas = {
+    ["UI-HUD-ActionBar-IconFrame-Slot"]={64, 31, 0.701172, 0.951172, 0.102051, 0.162598, false, false, "2x"},
+    ["UI-HUD-ActionBar-IconFrame"]={46, 22, 0.701172, 0.880859, 0.316895, 0.36084, false, false, "2x"},
+    ["UI-HUD-ActionBar-IconFrame-AddRow"]={51, 25, 0.701172, 0.900391, 0.215332, 0.265137, false, false, "2x"},
+    ["UI-HUD-ActionBar-IconFrame-Down"]={46, 22, 0.701172, 0.880859, 0.430176, 0.474121, false, false, "2x"},
+    ["UI-HUD-ActionBar-IconFrame-Flash"]={46, 22, 0.701172, 0.880859, 0.475098, 0.519043, false, false, "2x"},
+    ["UI-HUD-ActionBar-IconFrame-FlyoutBorderShadow"]={52, 26, 0.701172, 0.904297, 0.163574, 0.214355, false, false, "2x"},
+    ["UI-HUD-ActionBar-IconFrame-Mouseover"]={46, 22, 0.701172, 0.880859, 0.52002, 0.563965, false, false, "2x"},
+    ["UI-HUD-ActionBar-IconFrame-Border"]={46, 22, 0.701172, 0.880859, 0.361816, 0.405762, false, false, "2x"},
+    ["UI-HUD-ActionBar-IconFrame-AddRow-Down"]={51, 25, 0.701172, 0.900391, 0.266113, 0.315918, false, false, "2x"},
+}
+
+local function RemapTexture(texture)
+    local atlasId = texture:GetAtlas()
+    local atlas = replacementAtlas[atlasId]
+
+    -- don't even attempt to remap if the atlas is missing
+    if atlas == nil then return end
+
+    local width = texture:GetWidth()
+    local height = texture:GetHeight()
+    texture:SetTexture(replacementTexture)
+    texture:SetTexCoord(atlas[3], atlas[4], atlas[5], atlas[6])
+    texture:SetWidth(width)
+    texture:SetHeight(height)
+end
+
 local function AdjustButtonOverlays(button)
     button.cooldown:SetAllPoints(button)
+    RemapTexture(button.HighlightTexture)
+    RemapTexture(button.CheckedTexture)
+    RemapTexture(button.SpellHighlightTexture)
+    RemapTexture(button.NewActionTexture)
+    RemapTexture(button.PushedTexture)
+    RemapTexture(button.Border)
 end
 
 local function HideSelf(self)
